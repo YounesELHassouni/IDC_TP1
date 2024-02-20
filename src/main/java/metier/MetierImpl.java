25420e1bf8d99fb1492e5c6117b1926cb6609428
elhassouni <younesselhassouni18@gmail.com>
2024-02-20 22:01:20 +0100

    fourt Commit

--- src/main/java/metier/MetierImpl.java
+++ src/main/java/metier/MetierImpl.java
@@ -1,11 +1,17 @@
 package metier;
 
 import dao.IDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
+@Component
 public class MetierImpl implements IMetier {
     // Couplage faible
+
     private IDao dao;
-    public MetierImpl (IDao dao) {
+
+    public MetierImpl(IDao dao) {
         this.dao = dao;
     }
     @Override
--- src/main/java/presntation/Presentation.java
+++ src/main/java/presntation/Presentation.java
@@ -9,8 +9,8 @@ public class Presentation {
         Injection des dependances par instanciation static => new
          */
         DaoImpl2 dao = new DaoImpl2();
-        MetierImpl metier = new MetierImpl();
-        metier.setDao(dao);
+        MetierImpl metier = new MetierImpl(dao);
+        //metier.setDao(dao);
         System.out.println("Resultat : "+ metier.calcul());
     }
 }
